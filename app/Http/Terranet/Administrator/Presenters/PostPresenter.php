61e03b557f98487120725d4ee8e1bd330ea89240
gg <gutug84@gmail.com>
2020-07-21 13:44:12 +0300

    rename route

--- app/Http/Terranet/Administrator/Presenters/PostPresenter.php
+++ app/Http/Terranet/Administrator/Presenters/PostPresenter.php
@@ -8,7 +8,7 @@ class PostPresenter extends Presenter
 {
     public function title()
     {
-        return link_to_route('scaffold.view', $this->presentable->title, [
+        return link_to_route('cms', $this->presentable->title, [
             'module' => 'posts',
             'id' => $this->presentable
         ]);
